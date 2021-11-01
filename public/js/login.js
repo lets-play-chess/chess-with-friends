17440203276583fb30d5132d335430ada0daf833
CarsdanDvorachek <cdvchek@gmail.com>
2021-11-01 16:07:48 -0400

    added form functionality to the home page

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,53 @@
 const loginForm = document.querySelector("#login-form");
 
-//TODO: collect data from login form, make post request to log in user.  
-//TODO: once you get an ok response, redirect to the profile page
-
 loginForm.addEventListener("submit",(e)=>{
     e.preventDefault();
-    const userObj={
-        email:document.querySelector("#email").value,
-        password:document.querySelector("#password").value,
+    const loginObj={
+        email:document.querySelector("#email-login").value,
+        password:document.querySelector("#password-login").value,
     }
-    fetch("/api/users/login",{
+
+    // TODO: add the correct route to login
+    fetch("correct route to login",{
         method:"POST",
-        body:JSON.stringify(userObj),
+        body:JSON.stringify(loginObj),
         headers:{
             "Content-Type":"application/json"
         }
     }).then(res=>{
         if(res.ok){
-           location.href = "/profile"
+            // TODO: add the correct route to the user profile page
+            location.href = "correct user profile route"
         } else {
             alert("trumpet sound")
         }
-    })
-})
+    });
+});
+
+const signupForm = document.querySelector("#signup-form");
+
+
+signupForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const signupObj={
+        email:document.querySelector("#email-signup").value,
+        password:document.querySelector("#password-signup").value,
+        username:document.querySelector("#username-signup").value,
+    }
+
+    // TODO: add the correct route to signup
+    fetch("correct route to signup",{
+        method:"POST",
+        body:JSON.stringify(signupObj),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        if(res.ok){
+            // TODO: add the correct route to the user profile page
+            location.href = "correct user profile route"
+        } else {
+            alert("trumpet sound")
+        }
+    });
+});
